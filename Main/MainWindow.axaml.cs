3be3a617112a1b58b56b9da1c3bef58ba9091e81
Marco Buzzanca <marco.bzn@gmail.com>
2026-04-12 18:39:36 +0200

    Initial Commit

---
+++ Main/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace GogGalaxyCardViewer.Main;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Main/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GogGalaxyCardViewer.Main;
+
+public class MainWindowViewModel : ObservableObject
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
