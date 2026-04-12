f29d549f4620ebabf9b3059edee50ded01887d88
Marco Buzzanca <marco.bzn@gmail.com>
2026-04-12 21:11:40 +0200

    Improve loading logic

--- Main/MainWindowViewModel.cs
+++ Main/MainWindowViewModel.cs
@@ -2,40 +2,42 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using GogGalaxyCardViewer.Scan;
 
 namespace GogGalaxyCardViewer.Main;
 
-public class MainWindowViewModel(IMessenger messenger) : ObservableRecipient(messenger), IRecipient<ImageFoundMessage>
+public partial class MainWindowViewModel(IMessenger messenger)
+    : ObservableRecipient(messenger), IRecipient<ImageFoundMessage>
 {
     private const int ReceiveThreshold = 200;
     private const int LoadAmount = 50;
 
     private readonly List<string> _allImages = [];
-    private int _loadedImages;
 
-    public ObservableCollection<string> Images { get; } = [];
+    public ObservableCollection<string> LoadedImages { get; } = [];
+
 
     public void Receive(ImageFoundMessage message)
     {
         _allImages.Add(message.Path);
 
-        if (_loadedImages < ReceiveThreshold)
-        {
-            Images.Add(message.Path);
-            _loadedImages++;
-        }
+        if (LoadedImages.Count < ReceiveThreshold)
+            LoadedImages.Add(message.Path);
+    }
+
+    [RelayCommand]
+    private void FilterImages(string? filter)
+    {
     }
 
     public void LoadMoreItems()
     {
-        var limit = Math.Min(_allImages.Count, _loadedImages + LoadAmount);
+        var limit = Math.Min(_allImages.Count, LoadedImages.Count + LoadAmount);
 
-        for (var i = _loadedImages; i < limit; i++)
-            Images.Add(_allImages[i]);
-
-        _loadedImages = limit;
+        for (var i = LoadedImages.Count; i < limit; i++)
+            LoadedImages.Add(_allImages[i]);
     }
 
     protected override void OnActivated()
